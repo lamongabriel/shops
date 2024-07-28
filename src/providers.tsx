208f1b6819c40e5bc32315da7f05e2bfcb2dcdb8
Gabriel Lamon <lamongabriel@icloud.com>
2024-07-28 15:49:14 -0300

    added react query + api wrapper

--- src/providers.tsx
+++ src/providers.tsx
@@ -1,17 +1,23 @@
+import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactElement } from 'react'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { Toaster } from 'sonner'
 
 import { ThemeProvider } from '@/components/theme/theme-provider'
 
+import { queryClient } from './lib/react-query'
+
 export function Providers({ children }: { children: ReactElement }) {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="shops@theme">
-      <HelmetProvider>
+    <HelmetProvider>
+      <ThemeProvider defaultTheme="dark" storageKey="shops@theme">
         <Helmet titleTemplate="%s | shops" />
         <Toaster position="bottom-center" closeButton duration={3000} />
-        {children}
-      </HelmetProvider>
-    </ThemeProvider>
+
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </ThemeProvider>
+    </HelmetProvider>
   )
 }
